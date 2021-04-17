47ff080929728a2e716b6b1a8270d243d9a6bd18
mariiasolo <78789270+mariiasolo@users.noreply.github.com>
2021-04-17 23:05:02 +0300

    Create sample.js

---
+++ scripts/sample.js
@@ -0,0 +1,3 @@
+// We can actually create smth cool. Just don't know what =) 
+alert("Hello Dear Visitor! This is a collaboration project of students from IUPUI and Haaga-Helia Universities");
+close;
