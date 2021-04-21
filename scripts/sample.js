7fb45ded6da03b1e713b189443558669ca821d2a
mariiasolo <78789270+mariiasolo@users.noreply.github.com>
2021-04-21 23:32:20 +0300

    Update sample.js

--- scripts/sample.js
+++ scripts/sample.js
@@ -1,3 +1,35 @@
-// We can actually create smth cool. Just don't know what =) 
-alert("Hello Dear Visitor! This is a collaboration project of students from IUPUI and Haaga-Helia Universities");
-close;
+/* I can't make it work. Doing something here wrong. 
+So, I am using for now William's example of that function to make menu disappear 
+
+function myFunction() {
+
+	var x = document.getElementById("pull");
+	if (window.matchMedia("(max-width: 600px)").matches) {
+	  x.style.display = "block";
+	} else {
+	  x.style.display = "grid";
+	}
+  }
+
+*/
+
+
+$(function () {
+	var pull = $('#pull');
+	menu = $('nav ul');
+	menuHeight = menu.height();
+
+	$(pull).on('click', function (e) {
+		e.preventDefault();
+		menu.slideToggle();
+	});
+
+	$(window).resize(function () {
+		var w = $(window).width();
+		if (w > 600 && menu.is(':hidden')) {
+			menu.removeAttr('style');
+		}
+	});
+});
+
+
